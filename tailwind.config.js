0d3cde42a008ea666b75c26a334903186173b996
Priyam Bhattacharya <priyambhattacharya6@gmail.com>
2021-06-09 21:49:17 +0530

    Initial Commit

---
+++ tailwind.config.js
@@ -0,0 +1,43 @@
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      spacing: {
+        1000: '1000px',
+        940: '940px',
+        594: '594px',
+        253: '253px',
+        23: '23px' ,
+        26: '26px' ,
+        18: '18px',
+        67: '67px',
+        30: '30px',
+      },
+      colors: {
+        'navc': '#2b6198',
+        'active': '#fbcd73',
+        'content': '#444444',
+        'sp': '#fdd689',
+        'btbg': '#656770',
+        'list': '#1b5fa4',
+        'readmore': '#ba4004',
+      },
+      fontFamily: {
+        'sans': 'Tahoma, Verdana, Arial, sans-serif',
+      },
+      fontSize: {
+        '17': '17px',
+        '13': '13px',
+        '15': '15px',
+        '19': '19px',
+        '55': '55px',
+        '33': '33px',
+      }
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
